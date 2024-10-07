e5195b2a25768f8c6b82b63ef4383f57c7adf973
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-10-07 11:40:25 +0530

    calculater class created

---
+++ src/main/java/mypkg/MyCalc.java
@@ -0,0 +1,22 @@
+package mypkg;
+
+public class MyCalc {
+	
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	public int diff(int a, int b)
+	{
+		return a-b;
+	}
+
+	public static void main(String[] args) {
+		
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is: "+calc.sum(20, 10));
+		System.out.println("Diff is: "+calc.diff(20, 10));	
+
+	}
+
+}
