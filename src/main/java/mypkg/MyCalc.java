f0c7c236c9098702f6d02b656eeddeb62cc70728
hiteshupes <99481703+hiteshupes@users.noreply.github.com>
2024-10-07 12:25:09 +0530

    Update MyCalc.java

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -10,9 +10,9 @@ public class MyCalc {
 	{
 		return a-b;
 	}
-	public int mul(int a, int b)
+	public int div(int a, int b)
 	{
-		return a*b;
+		return a/b;
 	}
 
 	public static void main(String[] args) {
